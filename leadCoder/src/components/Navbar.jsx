89c2b4a3a6d6d8b78df0e098d47a96f02083618b
SharwanKunwar <sharwankunwar07@gmail.com>
2025-10-19 21:28:58 +0545

    updated dashboard

--- leadCoder/src/components/Navbar.jsx
+++ leadCoder/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import 'remixicon/fonts/remixicon.css'
 
 export default function Navbar() {
 return (
-    <div className=" text-white p-4 text-center text-lg bg-red-100">
+    <div className=" text-white text-center text-lg ">
         {/* holding left and right div  */}
-        <div className=" py-2 h-full  flex justify-between border-b-1 border-gray-400/30 bg-yellow-400">
+        <div className=" py-5 h-full  flex justify-between border-b-1 border-gray-400/30  px-15 mx-10">
             {/* left div  */}
             <div className="flex gap-5"> 
-                <div className="flex justify-center items-center gap-1">
-                    <img src="/LeadCoder.png" alt="img" className="h-[30px] w-[30px] p-1 rounded-full bg-white" /> 
+                <div className="flex justify-center items-center gap-3">
+                    <img src="/LeadCoder.png" alt="img" className="h-[30px] w-[30px] p-1 rounded-full bg-sky-400" /> 
                     <h1 className="text-[20px] font-bold">LeadCoder</h1>
                 </div>
-                <nav className=" flex justify-center items-center ">
+                <nav className=" flex justify-center items-center text-neutral-400">
                     <ul className="flex justify-center items-center gap-5 ">
                         <li>Dashboard</li>
                         <li>Concepts</li>
@@ -21,14 +21,14 @@ return (
                 </nav>
             </div>
             {/* right div  */}
-            <div className="flex justify-center items-center gap-3 text-[#8B8D93]">
-                <div className="bg-[#233648] rounded-lg w-[250px] h-[40px] flex justify-start items-center gap-3 ">
-                    <div className=" rounded-full pl-3 w-[40px] h-[40px] flex justify-center items-center"><i className="ri-search-line text-[#8B8D93"></i></div>
+            <div className="flex justify-center items-center gap-7 text-[#8B8D93]">
+                <div className="bg-[#233648] rounded-lg w-[300px] h-[40px] flex justify-start items-center gap-3 mastWhiteShadow">
+                    <div className=" rounded-full pl-3 w-[50px] h-[50px] flex justify-center items-center"><i className="ri-search-line text-[#8B8D93"></i></div>
                     <div>
-                        <input type="text" placeholder='Search'/>
+                        <input type="text" placeholder='Search' className='border-none outline-none'/>
                     </div>
                 </div>
-                <button className="bg-white rounded-full w-[40px] h-[40px]">a</button>
+                <button className="bg-white rounded-full w-[40px] h-[40px] mastWhiteShadow">a</button>
             </div>
         </div>
     </div>
--- leadCoder/src/pages/Dashboard.jsx
+++ leadCoder/src/pages/Dashboard.jsx
@@ -1,10 +1,113 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Dashboard() {
+  const [user, setUser] = useState("Sharwan kunwar");
   return (
-    <div>
-      this is dashboard
-    </div>
+    <>
+      <div className='w-full  h-screen flex gap-10 px-15 overflow-y-scroll no-scrollbar'>
+
+        {/* left div  ------------------------------------------------------------------------------*/}
+        <div className='w-[65%] py-10'>
+
+          <div className='w-full h-[30vh] rounded-2xl flex justify-center items-end pb-10 mastWhiteShadow' style={{background: 'linear-gradient(44deg,#3d5c4d,#acb167)'}}>
+            <h1 className='text-3xl text-white font-medium'>Welcome back, {user} You are a Code Master</h1>
+          </div>
+
+          <div className='mt-10 h-[50vh] py-1 px-1 overflow-y-scroll no-scrollbar'>
+            <h1 className='text-white text-2xl font-medium'>Contunue Learning</h1>
+            <div className=' mt-10 grid grid-cols-2 grid-row-2 gap-7'>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+          </div>
+          </div>
+
+          <div className='mt-10'>
+            <h1 className='text-white text-2xl font-medium'>Recommended for You</h1>
+            <div className=' mt-10 grid grid-cols-3 grid-row-2 gap-7'>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            <div className='bg-[#1A2633] h-[170px] rounded-xl mastWhiteShadow'>a</div>
+            
+          </div>
+          </div>
+
+          <div className='h-[20vh]'></div>
+
+          
+        </div>
+
+        {/* right div  ------------------------------------------------------------------------------*/}
+        <div className='w-[35%] py-10 grid grid-cols-1 grid-row-1 gap-10'>
+          
+          {/* progress div  */}
+          <div className='w-full h-[45vh] rounded-2xl bg-[#1A2633] mastWhiteShadow flex flex-col justify-center items-center py-5 px-10'>
+            <h1 className='text-3xl'>Overall Progress</h1>
+            <div className='w-full h-[100%] flex justify-center items-center'>
+              <div className='w-[200px] h-[200px] bg-white rounded-full flex justify-center items-center text-black'>a</div>
+            </div>
+            <p>"The secret of getting ahead is getting started."</p>
+          </div>
+
+          {/* projects div */}
+          <div className='w-full h-[30vh] rounded-lg bg-[#1A2633] mastWhiteShadow overflow-y-scroll p-5 no-scrollbar'>
+            <h1 className='text-2xl font-medium mb-5'>Projects</h1>
+            <div className='grid grid-cols-1 grid-row-1 gap-3 '>
+              {/* project 1 */}
+              <div className='bg-black h-[70px] rounded-md flex justify-between items-center gap-5 p-3'>
+                <div className=' w-full h-[50px] flex justify-start items-center gap-3'>
+                  <div className='w-[50px] h-full bg-white rounded-lg'></div>
+                  <h1>Project name</h1>
+                </div>
+                <button className='text-blue-400 w-[100px] h-[30px] rounded-sm'>View</button>
+              </div>
+              
+              {/* project 2 */}
+              <div className='bg-black h-[70px] rounded-md flex justify-between items-center gap-5 p-3'>
+                <div className=' w-full h-[50px] flex justify-start items-center gap-3'>
+                  <div className='w-[50px] h-full bg-white rounded-lg'></div>
+                  <h1>Project name</h1>
+                </div>
+                <button className='text-blue-400 w-[100px] h-[30px] rounded-sm'>View</button>
+              </div>
+
+            </div>
+          </div>
+
+          {/* achivements  */}
+          <div className='w-full h-[30vh] rounded-lg bg-[#1A2633] mastWhiteShadow overflow-y-scroll p-5 no-scrollbar'>
+            <h1 className='text-2xl font-medium mb-5'>Achivement</h1>
+            <div className='grid grid-cols-3 grid-row-3 gap-3 '>
+
+              <div className='bg-black h-[140px] rounded-md flex flex-col justify-center items-center p-3 gap-1'>
+                <div className='bg-white w-[60px] h-[60px] rounded-md'></div>
+                <p>Good Job</p>
+              </div>
+
+              <div className='bg-black h-[140px] rounded-md flex flex-col justify-center items-center p-3 gap-1'>
+                <div className='bg-white w-[60px] h-[60px] rounded-md'></div>
+                <p>Good Job</p>
+              </div>
+
+              <div className='bg-black h-[140px] rounded-md flex flex-col justify-center items-center p-3 gap-1'>
+                <div className='bg-white w-[60px] h-[60px] rounded-md'></div>
+                <p>Good Job</p>
+              </div>
+
+              <div className='bg-black h-[140px] rounded-md flex flex-col justify-center items-center p-3 gap-1'>
+                <div className='bg-white w-[60px] h-[60px] rounded-md'></div>
+                <p>Good Job</p>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
+        </div>
+    </>
   )
 }
 
